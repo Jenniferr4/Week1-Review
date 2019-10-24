8ecfca808c12e73ec3488e25e5454c641f9cdd7f
Jennier Granados <Jennier.Granados@improving.com>
2019-10-24 11:38:13 -0500

    day one bootcamp

--- src/main/java/org/improving/Main.java
+++ src/main/java/org/improving/Main.java
@@ -1,7 +1,23 @@
 package org.improving;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        boolean loop = true;
+        while (loop) {
+            System.out.print(">");
+            var input = scanner.nextLine();
+
+            if(input.equalsIgnoreCase("Exit")){
+                System.out.println("Adieu mon ami");
+                loop = false;
+            }
+            else {
+                System.out.println(input);
+            }
+        }
     }
 }
